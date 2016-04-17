b31a3b9f227c656d87bd31769b41f7ad0f7a3e56
Riswanda N.S <root.devilscream@gmail.com>
2016-04-17 20:48:56 +0800

    first commit

---
+++ source/MyNotification/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MyNotification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.title = new System.Windows.Forms.Label();
+            this.avatar = new System.Windows.Forms.PictureBox();
+            this.content = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.avatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.title.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(84, 12);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(74, 20);
+            this.title.TabIndex = 2;
+            this.title.Text = "Your Title";
+            // 
+            // avatar
+            // 
+            this.avatar.BackColor = System.Drawing.Color.Transparent;
+            this.avatar.Location = new System.Drawing.Point(12, 15);
+            this.avatar.Name = "avatar";
+            this.avatar.Size = new System.Drawing.Size(66, 61);
+            this.avatar.TabIndex = 3;
+            this.avatar.TabStop = false;
+            // 
+            // content
+            // 
+            this.content.BackColor = System.Drawing.Color.Transparent;
+            this.content.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.content.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.content.ForeColor = System.Drawing.Color.White;
+            this.content.Location = new System.Drawing.Point(85, 32);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(225, 41);
+            this.content.TabIndex = 4;
+            this.content.Text = "Your content";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(305, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(333, 89);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.avatar);
+            this.Controls.Add(this.title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "MyNotification";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.avatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.PictureBox avatar;
+        private System.Windows.Forms.Label content;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ source/MyNotification/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyNotification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            var screen = Screen.FromPoint(this.Location);
+            this.Location = new Point(screen.WorkingArea.Right - this.Width - 10, screen.WorkingArea.Bottom - this.Height - 20);
+            base.OnLoad(e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.title.Text = File.ReadAllText(@Application.StartupPath + "\\config\\title.ini");
+            this.content.Text = File.ReadAllText(@Application.StartupPath + "\\config\\content.ini");
+            this.avatar.Image = Image.FromFile(@Application.StartupPath + "\\config\\avatar.png");
+            this.avatar.SizeMode = PictureBoxSizeMode.Zoom;
+            timer1.Enabled = true;
+            timer1.Interval = 6000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
